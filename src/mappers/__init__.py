4ad7d616102e4fc9955d012abb072e39df2fea21
Artem Malyshev <proofit404@gmail.com>
2019-06-05 04:38:42 +0300

    Convert into a package with a public API.

---
+++ src/mappers/__init__.py
@@ -0,0 +1,14 @@
+"""
+mappers
+-------
+
+This module implements Declarative mappers from ORM models to domain entities.
+
+:copyright: (c) 2019 dry-python team.
+:license: BSD, see LICENSE for more details.
+"""
+
+from ._mapper import Evaluated, Mapper
+
+
+__all__ = ["Evaluated", "Mapper"]
--- src/mappers/_mapper.py
+++ src/mappers/_mapper.py
@@ -1,13 +1,3 @@
-"""
-mappers
--------
-
-This module implements Declarative mappers from ORM models to domain entities.
-
-:copyright: (c) 2019 dry-python team.
-:license: BSD, see LICENSE for more details.
-"""
-
 import dataclasses
 import operator
 import typing
