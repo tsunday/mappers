61ea4582ce8e53534a12e5b122e92b806183ada8
Artem Malyshev <proofit404@gmail.com>
2019-06-07 07:31:10 +0300

    Exit with non zero code when there are doctest errors.

--- tests/helpers/mddoctest.py
+++ tests/helpers/mddoctest.py
@@ -1,3 +1,4 @@
+import sys
 from doctest import testfile
 from glob import glob
 
@@ -10,5 +11,8 @@ def main():
     apps.populate(settings.INSTALLED_APPS)
     management.call_command("migrate")
     markdown_files = glob("**/*.md", recursive=True)
+    exit_code = 0
     for markdown_file in markdown_files:
-        testfile(markdown_file, module_relative=False)
+        failed, attempted = testfile(markdown_file, module_relative=False)
+        exit_code += failed
+    sys.exit(exit_code)
