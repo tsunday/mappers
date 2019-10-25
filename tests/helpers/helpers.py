7dedc5dd69870b2dbd53dbe07958238b77b2ca4d
Artem Malyshev <proofit404@gmail.com>
2019-10-25 19:07:25 +0300

    Test rules in different configs. (#106)
    * Test rules in different configs.
    
    * Test tox basepython match Azure task version.
    
    * Synchronize configs.
    
    * Reorder azure pipeline tasks.
    
    * Measure helpers test coverage.
    
    * Reorder Tox environments.
    
    * Correct tox deps algorithm.
    
    * Order tox deps.

---
+++ tests/helpers/helpers.py
@@ -0,0 +1,11 @@
+import configparser
+import textwrap
+
+
+def tox_info(var):
+    ini_parser = configparser.ConfigParser()
+    ini_parser.read("tox.ini")
+    for section in ini_parser:
+        if var in ini_parser[section]:
+            value = textwrap.dedent(ini_parser[section][var].strip())
+            yield section, value
