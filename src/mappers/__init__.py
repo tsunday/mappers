b2e12b77847afa60556d45deed242659132efff5
Artem Malyshev <proofit404@gmail.com>
2020-01-02 00:07:00 +0300

    Update copyright year. (#129)

--- src/mappers/__init__.py
+++ src/mappers/__init__.py
@@ -1,7 +1,7 @@
 """
 This module implements Declarative mappers from ORM models to domain entities.
 
-:copyright: (c) 2019 dry-python team.
+:copyright: (c) 2019-2020 dry-python team.
 :license: BSD, see LICENSE for more details.
 """
 from _mappers.factory import mapper_factory as Mapper
--- src/mappers/exceptions.py
+++ src/mappers/exceptions.py
@@ -1,7 +1,7 @@
 """
 This module contains the set of mappers' exceptions.
 
-:copyright: (c) 2019 by dry-python team.
+:copyright: (c) 2019-2020 by dry-python team.
 :license: BSD, see LICENSE for more details.
 """
 from _mappers.exceptions import MapperError
