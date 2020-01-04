9ed4ba6e7bba2c58c7681982c813cafc6142e57b
Artem Malyshev <proofit404@gmail.com>
2020-01-04 16:00:17 +0300

    refactor: reorder switch statement to increase coverage

--- src/_mappers/entities/__init__.py
+++ src/_mappers/entities/__init__.py
@@ -4,17 +4,17 @@ from _mappers.entities import pydantic
 
 
 def _entity_factory(entity):
-    if attrs._is_attrs(entity):
-        fields = attrs._get_fields(entity)
-        factory = attrs._get_factory(fields, entity)
-        return fields, factory
-    elif dataclasses._is_dataclass(entity):
+    if dataclasses._is_dataclass(entity):
         fields = dataclasses._get_fields(entity)
         factory = dataclasses._get_factory(fields, entity)
         return fields, factory
     elif pydantic._is_pydantic(entity):
         fields = pydantic._get_fields(entity)
         factory = pydantic._get_factory(fields, entity)
+        return fields, factory
+    elif attrs._is_attrs(entity):
+        fields = attrs._get_fields(entity)
+        factory = attrs._get_factory(fields, entity)
         return fields, factory
     else:
         raise Exception
